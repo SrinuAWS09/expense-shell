ba13698a5ba2b97052ef97c2f6d4afded327fbfa
Srinu <gollavillisrinu09@gmail.com>
2025-01-06 11:32:23 +0530

    sql-script for expense project

---
+++ mysql.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+NO=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOG_FOLDER="/var/log/expense_logs"
+LOG_FILE=$(echo $0 | cut -d "." -f2)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+echo "script started at : $TIMESTAMP" &>>$LOG_FILE_NAME
+
+validate(){
+    if [ $1 -ne 0 ]
+        then
+            echo -e "ERROR: Installation is $R failed $N...$2"
+            exit 1
+        else 
+            echo -e " Installation $G success $N.. $2"
+        fi
+}
+CHECK_ROOT(){
+
+if [ $NO -ne 0 ]
+then
+    echo "ERROR: This is not root user"
+    exit 1
+fi
+}
+
+dnf install mysql-server -y
+VALIDATE $? "Installing mysql server"
+systemctl enable mysqld
+VALIDATE $? "Enableing my sql server"
+ systemctl start mysqld
+ VALIDATE $? "star the mysql server"
+ mysql_secure_installation --set-root-pass ExpenseApp@1
+ VALIDATE $? "Setting Root password"
+ 
