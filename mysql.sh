6ff43dba6f933fcb6f69550d940ff3eea22ed34c
Srinu <gollavillisrinu09@gmail.com>
2025-01-06 11:40:31 +0530

    sql-script for expense project changes

--- mysql.sh
+++ mysql.sh
@@ -5,13 +5,13 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-LOG_FOLDER="/var/log/expense_logs"
+LOG_FOLDER="/var/log/expense-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f2)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 echo "script started at : $TIMESTAMP" &>>$LOG_FILE_NAME
 
-validate(){
+VALIDATE(){
     if [ $1 -ne 0 ]
         then
             echo -e "ERROR: Installation is $R failed $N...$2"
@@ -29,12 +29,11 @@ then
 fi
 }
 
-dnf install mysql-server -y
+dnf install mysql-server -y &>>$LOG_FILE_NAME
 VALIDATE $? "Installing mysql server"
-systemctl enable mysqld
-VALIDATE $? "Enableing my sql server"
- systemctl start mysqld
- VALIDATE $? "star the mysql server"
- mysql_secure_installation --set-root-pass ExpenseApp@1
+systemctl enable mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "Enableing my sql server" 
+ systemctl start mysqld &>>$LOG_FILE_NAME
+ VALIDATE $? "star the mysql server" 
+ mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG_FILE_NAME
  VALIDATE $? "Setting Root password"
- 
