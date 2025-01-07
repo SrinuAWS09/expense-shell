d39a9fbbc88deb2d881a752d0b8714e1b93c46ed
Srinu <gollavillisrinu09@gmail.com>
2025-01-07 19:59:24 +0530

    reverse shell script

--- Reverse.sh
+++ Reverse.sh
@@ -10,6 +10,8 @@ then
   cols=$(head -n1 "$filename" | wc -w)
   echo " cols data is: $cols"
   for ((i = 1; i <= cols; i++)); do
+  data1=$(cut -d' ' -f"$i" "$filename")
+  echo "data 1 is : $data1"
     data=$(cut -d' ' -f"$i" "$filename" | paste -sd' ' -)
     echo " data is : $data"
   done
