fe7ce5267bf6a227779c3e68595a7f57724daa7d
Srinu <gollavillisrinu09@gmail.com>
2025-01-07 18:55:32 +0530

    practice1 shell script

--- practice1.sh
+++ practice1.sh
@@ -2,6 +2,10 @@
 echo "Please enter the words to test:"
 read Given_words
 echo "Given words are:${Given_words}"
+
+echo "Please provide the file_name:"
+read file_name
+echo "File_Name is: ${file_name}"
  
 # if [ -f "$filename" ]
 #  then
