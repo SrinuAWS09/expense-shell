258e9c8e5530e6f2a1f3b94da7f98a65e1a46c3c
Srinu <gollavillisrinu09@gmail.com>
2025-01-07 18:59:29 +0530

    practice1 shell script

--- practice1.sh
+++ practice1.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-echo "Please enter the words to test:"
-read Given_words
-echo "Given words are:${Given_words}"
+# echo "Please enter the words to test:"
+# read Given_words
+# echo "Given words are:${Given_words}"
 
-echo "Please provide the file_name:"
-read file_name
-echo "File_Name is: ${file_name}"
+echo "Please provide the filename:"
+read filename
+echo "FileName is: ${filename}"
  
-# if [ -f "$filename" ]
-#  then
-#   echo "Processing file: $filename"
+ if [ -f "$filename" ]
+  then
+   echo "Processing file: $filename"
+   # Count word occurrences and display the top 5
+  tr -s '[:space:][:punct:]' '\n' < "$filename" | tr '[:upper:]' '[:lower:]' | sort | uniq -c | sort -nr | head -5
+else
+  echo "File does not exist. Please provide a valid file."
+fi
 #   else 
 #   echo "Something wrong somewhere"
 # fi
