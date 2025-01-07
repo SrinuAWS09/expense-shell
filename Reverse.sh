5dc428d9ce4d718b059ea835c8f6b0bf4a4c90cf
Srinu <gollavillisrinu09@gmail.com>
2025-01-07 19:22:57 +0530

    reverse shell script

---
+++ Reverse.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Transpose rows and columns using paste and cut
+
+echo "Enter the file name:"
+read filename
+
+if [ -f "$filename" ]; then
+  cols=$(head -n1 "$filename" | wc -w)
+  for ((i = 1; i <= cols; i++)); do
+    cut -d' ' -f"$i" "$filename" | paste -sd' ' -
+  done
+else
+  echo "File does not exist."
+fi
